5c14dcb1ea17ddc5e8aa3df5ecdc61b1ca6f3f3a
a13xl <106622336+a13xl@users.noreply.github.com>
2023-04-20 22:24:45 +0200

    updated projects und contact-form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-contact-form',
@@ -8,9 +9,74 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class ContactFormComponent {
   @ViewChild('contactForm') contactForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendBtn') sendBtn!: ElementRef;
 
+  validateInput(id) {
+    let field = this.checkInputName(id);
+    let error;
+
+    if(id == 'nameField') {
+      error = this.checkName(field);
+    } else if(id == 'emailField') {
+      error = this.checkEmail(field);
+    } else if(id == 'messageField') {
+      error = this.checkMessage(field);
+    } else {
+      // enable send button
+    }
+
+    if(error === true) {
+      field.classList.add('input-invalid');
+    }
+  }
+
+  resetInputError(id) {
+    let field = this.checkInputName(id);
+
+    field.classList.remove('input-invalid');
+  }
+
+  checkEmail(inputField) {
+    debugger;
+    let email = inputField.value.name.toLowerCase();
+
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  checkMessage(inputField) {
+    if(inputField.value.length > 10) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  checkName(inputField) {
+    if(inputField.value.length > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  checkInputName(id) {
+    switch (true) {
+      case id == 'nameField':
+        let nameField = this.nameField.nativeElement;
+        return nameField;
+      case id == 'emailField':
+        let emailField = this.emailField.nativeElement;
+          return emailField;
+      case id == 'messageField':
+        let messageField = this.messageField.nativeElement;
+          return messageField;
+      default:
+        break;
+    }
+  }
 
   async sendMail() {
     console.log('Sending Mail', this.contactForm);
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as projectData from 'src/assets/projects.json';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+  projects: any = projectData.default;
 
-  codeProject() {
-
+  ngOnInit(): void {
+    /* console.log(this.projects); */
   }
 
-  tryProject() {
-
-  }
 
 }
