2ef019ec16803929d0838b3c379de4a8ae7a9095
a13xl <general01@tuta.io>
2023-04-27 19:14:10 +0200

    added sort by category

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import * as projectData from 'src/assets/projects.json';
 
 @Component({
@@ -7,49 +7,50 @@ import * as projectData from 'src/assets/projects.json';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit{
-  // @ViewChild('allProjects') allProjects!: ElementRef;
+  @ViewChildren('categoryBtn', { read: ElementRef }) categoryBtn: QueryList<ElementRef>;
+  @ViewChild('categoryAllBtn') categoryAllBtn: ElementRef;
 
   projects: any = projectData.default;
+  projectsSort: Array<any> = [];
   categories: Array<any> = [];
 
   ngOnInit(): void {
+    this.projectsSort = this.projects;
+
+    // push all category once to category Array
     this.projects.forEach(project => {
       let category = project.category;
-
       if(!this.categories.includes(category)) {
         this.categories.push(category);
       }
     });
   }
 
-  sortProjectCategory(category: string) {
-    console.log('Sort to Category:', category)
+  sortProjectCategory(id: number, category: string) {
     this.enableBtns();
-  }
+    if(id == 99) { // all button
+      this.categoryAllBtn.nativeElement.disabled = true;
+    } else {
+      this.categoryBtn.toArray()[id].nativeElement.disabled = true;
+    }
 
-  enableBtns() {
-    this.categories.forEach(element => {
-      
+    this.projectsSort = [];
+    this.projects.forEach(project => {
+      if(category == project.category) {
+        this.projectsSort.push(project);
+      } else if (category == 'all') {
+        this.projectsSort = this.projects;
+      }
     });
   }
 
-/*   sortProjects(category: string) {
-    this.enableBtns();
-    console.log(category);
-
-    if(category == 'all') {
-      this.allProjects.nativeElement.disabled = true;
-    } else {
-      let projectname = category+'Projects';
-      //projectname.nativeElement.disabled = true;
-    }
+  enableBtns() {
+    this.categoryAllBtn.nativeElement.disabled = false;
+    this.categoryBtn.forEach((categoryBtn: ElementRef) => {
+      categoryBtn.nativeElement.disabled = false;
+    });
   }
 
-  enableBtns() {
-    this.allProjects.nativeElement.disabled = false;
-    this.JavaScriptProjects.nativeElement.disabled = false;
-    this.AngularProjects.nativeElement.disabled = false;
-  } */
 
 
 }
