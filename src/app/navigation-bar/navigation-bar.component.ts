ac788094dc58a88a17b4de407dafd416ca3d946e
a13xl <106622336+a13xl@users.noreply.github.com>
2023-05-01 11:01:15 +0200

    updated home link

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,12 +8,23 @@ import { Component } from '@angular/core';
 })
 export class NavigationBarComponent {
 
-  goToTop() {
-    window.scroll({
-      top: 0,
-      left:0,
-      behavior: 'smooth'
-    })
+  constructor(public router: Router) { }
+
+  scrollHeader() { // only scroll smooth when on same site
+    if(this.router.url === '/' || this.router.url.startsWith('/#')) {
+      window.scroll({
+        top: 0,
+        left:0,
+        behavior: 'smooth'
+      })
+    } else {
+      // jump to top
+      window.scrollTo(0, 0);
+    }
+  }
+
+  scrollTop() {
+    window.scrollTo(0, 0);
   }
 
 }
